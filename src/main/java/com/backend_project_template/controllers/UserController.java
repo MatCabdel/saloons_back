73d71d86ecf3b69370730b8fb24ac702a3312ebf
MatCabdel <chauveau.math@gmail.com>
2025-04-07 15:22:42 +0200

    try git config

--- src/main/java/com/backend_project_template/controllers/UserController.java
+++ src/main/java/com/backend_project_template/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.backend_project_template.controllers;
 
 import com.backend_project_template.Entity.User;
 import com.backend_project_template.service.UserService;
+import java.util.Objects;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,24 +12,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
 @RestController
 @RequestMapping("/profile")
 public class UserController {
-    private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+  private final UserService userService;
 
-    @GetMapping("/{email}")
-    public ResponseEntity<User> getUserProfile(@PathVariable String email, @AuthenticationPrincipal UserDetails userDetails) {
-        if(!Objects.equals(userDetails.getUsername(), email)) {
-            throw new AccessDeniedException("Access denied");
-        }
-        System.out.println(userDetails.getUsername());
-        User user = userService.findByEmail(email);
-        return ResponseEntity.ok(user);
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/{email}")
+  public ResponseEntity<User> getUserProfile(@PathVariable String email, @AuthenticationPrincipal UserDetails userDetails) {
+    if (!Objects.equals(userDetails.getUsername(), email)) {
+      throw new AccessDeniedException("Access denied");
     }
+    System.out.println(userDetails.getUsername());
+    User user = userService.findByEmail(email);
+    return ResponseEntity.ok(user);
+  }
 }
