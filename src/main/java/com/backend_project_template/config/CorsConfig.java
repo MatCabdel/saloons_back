1e8501617bd5fed0e3c3b3e3aacc5a0242ef7891
MatCabdel <chauveau.math@gmail.com>
2026-01-16 01:37:50 +0100

    annulation dernier commit

--- src/main/java/com/backend_project_template/config/CorsConfig.java
+++ src/main/java/com/backend_project_template/config/CorsConfig.java
@@ -3,7 +3,6 @@ package com.backend_project_template.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,12 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     System.out.println("CORS ALLOWED ORIGINS: " + allowedOrigins);
     String[] origins = allowedOrigins.split(",");
-    registry.addMapping("/**").allowedOrigins(origins).allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS");
-  }
-
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/images/**")
-        .addResourceLocations("file:uploads/images/");
+    registry.addMapping("/**").allowedOrigins(origins).allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD",
+        "OPTIONS");
   }
 }
