d866d19e73ebd3339ee57b52a973d6010bd84576
MatCabdel <chauveau.math@gmail.com>
2026-01-16 01:29:21 +0100

    fix: add resource handler to serve static images from uploads folder

--- src/main/java/com/backend_project_template/config/CorsConfig.java
+++ src/main/java/com/backend_project_template/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.backend_project_template.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,11 @@ public class CorsConfig implements WebMvcConfigurer {
     System.out.println("CORS ALLOWED ORIGINS: " + allowedOrigins);
     String[] origins = allowedOrigins.split(",");
     registry.addMapping("/**").allowedOrigins(origins).allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/images/**")
+        .addResourceLocations("file:uploads/images/");
   }
 }
